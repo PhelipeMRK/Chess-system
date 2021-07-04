177ee77a3992d35dfee39ee6481d1057f8267f8d
PhelipeMRK <phelipe.mrk0@yahoo.com.br>
2021-07-04 20:49:25 -0300

    creating Chess Layer and printing board function

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,11 @@
 package application;
 
+import chessLayer.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
-	}
-
-}
+		ChessMatch chessMatch = new ChessMatch();	
+		UI.printBoard(chessMatch.getPieces());
+}}
