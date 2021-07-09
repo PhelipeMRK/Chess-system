01833fd74535308ce796c47c3b37cc223041ea40
PhelipeMRK <phelipe.mrk0@yahoo.com.br>
2021-07-09 05:55:10 -0300

    Added checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -39,14 +39,12 @@ public class Program {
 			
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
+				sc.nextLine();}
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-			
+				sc.nextLine();}
 		}
-
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
