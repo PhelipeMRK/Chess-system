c78d2283d387da479a2f053587cf359a479bc1fd
PhelipeMRK <phelipe.mrk0@yahoo.com.br>
2021-07-10 20:06:04 -0300

    Added the piece 'Pawn'

--- src/chessLayer/ChessMatch.java
+++ src/chessLayer/ChessMatch.java
@@ -8,6 +8,7 @@ import boardLayer.Board;
 import boardLayer.Piece;
 import boardLayer.Position;
 import chessLayer.pieces.King;
+import chessLayer.pieces.Pawn;
 import chessLayer.pieces.Rook;
 
 public class ChessMatch {
@@ -147,8 +148,28 @@ public class ChessMatch {
 		piecesOnTheBoard.add(piece);}
 	
 	private void initialSetup() {
-		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('d', 8, new King(board, Color.WHITE));
-		
-		placeNewPiece('f', 1, new King(board, Color.BLACK));}
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));}
 }
+
