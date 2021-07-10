a725cee92abe7ff24ec6dcccbb236a88896063d7
PhelipeMRK <phelipe.mrk0@yahoo.com.br>
2021-07-10 20:40:34 -0300

    Added the piece 'Queen'

--- src/chessLayer/ChessMatch.java
+++ src/chessLayer/ChessMatch.java
@@ -11,6 +11,7 @@ import chessLayer.pieces.Bishop;
 import chessLayer.pieces.King;
 import chessLayer.pieces.Knight;
 import chessLayer.pieces.Pawn;
+import chessLayer.pieces.Queen;
 import chessLayer.pieces.Rook;
 
 public class ChessMatch {
@@ -155,6 +156,7 @@ public class ChessMatch {
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
@@ -169,6 +171,7 @@ public class ChessMatch {
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
